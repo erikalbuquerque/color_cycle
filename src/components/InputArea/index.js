b5b77ab8c01c5b6b323a5f26d1b44f6da9e00564
erikalbuquerque <eriksilv.77@gmail.com>
2020-08-26 12:29:37 -0300

    Refactore: isValid

--- src/components/InputArea/index.js
+++ src/components/InputArea/index.js
@@ -15,14 +15,24 @@ function InputArea(){
   const keywords = ["a", "b", "c", "d", "e", "f"];
   const keynumbers = ["0", "1", "2", "3", "4", "6", "7", "8", "9"];
 
+  function toUpper(value){
+    return value.toUpperCase()
+  }
+
   function isValide(value){
     const valueInArray = value.split("");
+
+    const arrayUpperCase = valueInArray.map(word => toUpper(word));
+
     valueInArray.map(value => {
+
+      const isWordUpperCase = arrayUpperCase.includes(value);
       
       const isWord = keywords.includes(value);
+
       const isNumber = keynumbers.includes(value);
 
-      if(!isWord && !isNumber) { 
+      if((!isNumber && !isWord) && (!isWordUpperCase || isWordUpperCase)) { 
         setMessage("Invalide input!");
         return setShowMessage(true);
       }
