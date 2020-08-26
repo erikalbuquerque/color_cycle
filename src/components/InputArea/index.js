228e4f60e4f76a616f8dafbc50e76cfcbb81b0d9
erikalbuquerque <eriksilv.77@gmail.com>
2020-08-26 11:47:48 -0300

    Feature: isValid Check that the user input is valid

--- src/components/InputArea/index.js
+++ src/components/InputArea/index.js
@@ -1,4 +1,4 @@
-ginish daimport React from "react";
+import React, { useState } from "react";
 
 import { Container, Hashtag, InputContent } from "./styles";
 
@@ -9,16 +9,41 @@ import { useHex } from "../../context/Hexadecimal";
 function InputArea(){
   const { setRed, setGreen, setBlue } = useHex();
 
+  const [showMessage, setShowMessage] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const keywords = ["a", "b", "c", "d", "e", "f"];
+  const keynumbers = ["0", "1", "2", "3", "4", "6", "7", "8", "9"];
+
+  function isValide(value){
+    const valueInArray = value.split("");
+    valueInArray.map(value => {
+      
+      const isWord = keywords.includes(value);
+      const isNumber = keynumbers.includes(value);
+
+      if(!isWord && !isNumber) { 
+        setMessage("Invalide input!");
+        return setShowMessage(true);
+      }
+      return setShowMessage(false);
+    })
+    
+  }
+
   function changeRed(value){
-    setRed(value)
+    isValide(value);
+    setRed(value);
   }
 
   function changeGreen(value){
-    setGreen(value)
+    isValide(value);
+    setGreen(value);
   }
 
   function changeBlue(value){
-    setBlue(value)
+    isValide(value);
+    setBlue(value);
   }
 
   return(
@@ -28,20 +53,23 @@ function InputArea(){
       <InputContent 
         name="hex01" 
         placeholder="00" 
+        maxLength="2"
         onChange={(e) => changeRed(e.target.value)}
       />
       <InputContent 
         name="hex01" 
         placeholder="00" 
+        maxLength="2"
         onChange={(e) => changeGreen(e.target.value)}
       />
       <InputContent 
         name="hex01" 
         placeholder="00" 
+        maxLength="2"
         onChange={(e) => changeBlue(e.target.value)}
       />
       
-      
+      {showMessage ? <span>{message}</span> : ""}
 
     </Container>
   );
